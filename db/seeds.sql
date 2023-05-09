669b7729605537840d08ad500be4b0b75c6758ee
Danny Mak <danny.mak2@gmail.com>
2023-05-09 14:34:27 -0500

    changed seed data so adding roles dont duplicate roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,22 +1,18 @@
 INSERT INTO department (id, name)
 VALUES (1, "Sales"),
-       (2, "Sales"),
-       (3, "Engineering"),
-       (4, "Engineering"),
-       (5, "Finance"),
-       (6, "Finance"),
-       (7, "Legal"),
-       (8, "Legal");
+       (2, "Engineering"),
+       (3, "Finance"),
+       (4, "Legal");
 
 INSERT INTO role (id, title, salary, department_id)
 VALUES (1, "Sales Lead", 100000, 1),
-       (2, "Salesperson", 80000, 2),
-       (3, "Lead engineer", 150000, 3),
-       (4, "Software Engineer", 120000, 4),
-       (5, "Account Manager", 160000, 5),
-       (6, "Accountant", 125000, 6),
-       (7, "Legal Team Lead", 250000, 7),
-       (8, "Lawyer", 190000, 8);
+       (2, "Salesperson", 80000, 1),
+       (3, "Lead engineer", 150000, 2),
+       (4, "Software Engineer", 120000, 2),
+       (5, "Account Manager", 160000, 3),
+       (6, "Accountant", 125000, 3),
+       (7, "Legal Team Lead", 250000, 4),
+       (8, "Lawyer", 190000, 4);
        
 INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
 VALUES (1, "John", "Doe", 1, NULL),
